acc4591aad82cced4e37f57c2db78b80000bbd73
luca <gyeongjae.h.dev@gmail.com>
2023-10-24 16:30:29 +0900

    #1 - 음료 여러잔을 추가하는 기능 추가
    음료 여러 잔을 추가하는 기능 추가
    테스트 코드에서 해피 케이스를 테스트하기 위해서 경계값 테스트를 했고, 예외 케이스를 테스트했다

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -30,6 +30,29 @@ class CafeKioskTest {
 		assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
 	}
 
+	@DisplayName("음료를 여러 잔 추가한다")
+	@Test
+	void addSeveralBeverages() {
+		CafeKiosk cafeKiosk = new CafeKiosk();
+		Americano americano = new Americano();
+
+		cafeKiosk.add(americano, 2);
+
+		assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+		assertThat(cafeKiosk.getBeverages().get(1)).isEqualTo(americano);
+	}
+
+	@DisplayName("음료를 0잔 추가하면 에러가 발생한다")
+	@Test
+	void addZeroBeverages() {
+		CafeKiosk cafeKiosk = new CafeKiosk();
+		Americano americano = new Americano();
+
+		assertThatThrownBy(() -> cafeKiosk.add(americano, 0))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("음료는 1잔 이상 주문하실 수 있습니다");
+	}
+
 	@DisplayName("음료를 삭제한다")
 	@Test
 	void remove() {
