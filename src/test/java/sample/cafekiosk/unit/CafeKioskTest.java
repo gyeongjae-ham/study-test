562c3866d520b7852c3b9b060944743ab8b80d5b
luca <gyeongjae.h.dev@gmail.com>
2023-10-24 17:52:58 +0900

    :white_check_mark: Add pass test in BDD style

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -84,6 +84,24 @@ class CafeKioskTest {
 		assertThat(cafeKiosk.getBeverages()).isEmpty();
 	}
 
+	@DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다")
+	@Test
+	void calculateTotalPrice() {
+		// given
+		CafeKiosk cafeKiosk = new CafeKiosk();
+		Americano americano = new Americano();
+		Latte latte = new Latte();
+
+		cafeKiosk.add(americano);
+		cafeKiosk.add(latte);
+
+		// when
+		int totalPrice = cafeKiosk.calculateTotalPrice();
+
+		// then
+		assertThat(totalPrice).isEqualTo(8500);
+	}
+
 	@DisplayName("영업 시간에 주문하면 주문이 된다.")
 	@Test
 	void createOrderWithCurrentTime() {
