2e6f1fe68e0ae5ea342cb6460b841ef1af57e9eb
luca <gyeongjae.h.dev@gmail.com>
2023-10-25 10:19:56 +0900

    #8 - Persistance Layer Test 작성
     - Persistance Layer Test 작성

---
+++ src/test/java/sample/cafekiosk/spring/domain/product/ProductRepositoryTest.java
@@ -0,0 +1,65 @@
+package sample.cafekiosk.spring.domain.product;
+
+import static org.assertj.core.api.Assertions.*;
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
+import static sample.cafekiosk.spring.domain.product.ProductType.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+// @SpringBootTest
+@DataJpaTest // @SpringBootTest에 비해서 Jpa 관련 bean들만 사용하기 때문에 속도가 빠릅니다
+class ProductRepositoryTest {
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@DisplayName("원하는 판매 상태를 가진 상품들을 조회한다.")
+	@Test
+	void findAllBySellingStatusIn() {
+		// given
+		Product product1 = Product.builder()
+			.productNumber("001")
+			.type(HANDMADE)
+			.sellingStatus(SELLING)
+			.name("아메리카노")
+			.price(4000)
+			.build();
+		Product product2 = Product.builder()
+			.productNumber("002")
+			.type(HANDMADE)
+			.sellingStatus(HOLD)
+			.name("카페라떼")
+			.price(4500)
+			.build();
+		Product product3 = Product.builder()
+			.productNumber("003")
+			.type(HANDMADE)
+			.sellingStatus(STOP_SELLING)
+			.name("팥빙수")
+			.price(7000)
+			.build();
+		productRepository.saveAll(List.of(product1, product2, product3));
+
+		// when
+		List<Product> products = productRepository.findAllBySellingStatusIn(List.of(SELLING, HOLD));
+
+		// then
+		assertThat(products).hasSize(2)
+			// 테스트 하고자 하는 필드만 추출할 수 있다
+			.extracting("productNumber", "name", "sellingStatus")
+			// containsExactly: 순서까지 맞는지 확인해 준다
+			// containsExactlyInAnyOrder: 순서 상관없이 확인해 준다.
+			.containsExactlyInAnyOrder(
+				tuple("001", "아메리카노", SELLING),
+				tuple("002", "카페라떼", HOLD)
+			);
+	}
+
+}
