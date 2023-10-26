2512a7365e40f41641f1f4dd2944c473f2024ff2
luca <gyeongjae.h.dev@gmail.com>
2023-10-26 18:00:57 +0900

    #10 - @SpringBootTest, @DataJpaTest 차이점, @AfterEach 추가
    - @SpringBootTest, @DataJpaTest 차이는 빈을 등록하는 범위도 있지만, @Transactional 차이도 있다
    - @SpringBootTest는 @Transactional이 없기 때문에 @AfterEach로 테스트들간의 간섭을 해결해준다

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -7,6 +7,7 @@ import static sample.cafekiosk.spring.domain.product.ProductType.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,19 +16,35 @@ import org.springframework.test.context.ActiveProfiles;
 
 import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.domain.order.OrderRepository;
+import sample.cafekiosk.spring.domain.orderproduct.OrderProductRepository;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductType;
 
 @ActiveProfiles("test")
 @SpringBootTest
-	// @DataJpaTest
 class OrderServiceTest {
 
 	@Autowired
 	private ProductRepository productRepository;
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	@Autowired
+	private OrderProductRepository orderProductRepository;
+
 	@Autowired
 	private OrderService orderService;
+
+	@AfterEach
+	void tearDown() {
+		// productRepository.deleteAll();
+		orderProductRepository.deleteAllInBatch();
+		productRepository.deleteAllInBatch();
+		orderRepository.deleteAllInBatch();
+	}
 
 	@DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
 	@Test
